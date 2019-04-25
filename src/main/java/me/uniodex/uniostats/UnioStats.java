208c91d2d1f26f3e54f563cfc63d097f758b5f16
Yiğit Duman <yigitq@gmail.com>
2019-04-25 15:53:08 +0300

    Initial commit

---
+++ src/main/java/me/uniodex/uniostats/UnioStats.java
@@ -0,0 +1,16 @@
+package me.uniodex.uniostats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UnioStats extends JavaPlugin {
+
+
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
